a80bbccdd2792a43e131763d80d9c9e65e988619
pratikpnikam <pratikpnikam14@gmail.com>
2019-08-17 22:00:13 +0530

    Add project files.

---
+++ ADOConnectedDemo1/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace ADOConnectedDemo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEmp = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtDesg = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(12, 391);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(89, 35);
+            this.btnInsert.TabIndex = 0;
+            this.btnInsert.Text = "Insert Emp";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(139, 392);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(134, 34);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "Update City";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(309, 391);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(89, 35);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete Emp";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEmp
+            // 
+            this.btnEmp.Location = new System.Drawing.Point(451, 391);
+            this.btnEmp.Name = "btnEmp";
+            this.btnEmp.Size = new System.Drawing.Size(89, 35);
+            this.btnEmp.TabIndex = 3;
+            this.btnEmp.Text = "View Emp";
+            this.btnEmp.UseVisualStyleBackColor = true;
+            this.btnEmp.Click += new System.EventHandler(this.btnEmp_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(156, 53);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 22);
+            this.txtId.TabIndex = 4;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(156, 106);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 22);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtDesg
+            // 
+            this.txtDesg.Location = new System.Drawing.Point(156, 163);
+            this.txtDesg.Name = "txtDesg";
+            this.txtDesg.Size = new System.Drawing.Size(100, 22);
+            this.txtDesg.TabIndex = 6;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(156, 224);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 22);
+            this.txtCity.TabIndex = 9;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(156, 294);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 22);
+            this.txtAge.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Employee ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Employee Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Employee Desg";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 229);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Employee City";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 297);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 17);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Employee Age";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(285, 56);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(503, 260);
+            this.listBox1.TabIndex = 16;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(592, 391);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(122, 35);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtDesg);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnEmp);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEmp;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtDesg;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ ADOConnectedDemo1/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace ADOConnectedDemo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@" Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\user\source\repos\ADOConnectedDemo1\ADOConnectedDemo1\DatabaseEmp.mdf;Integrated Security=True;User Instance=True");
+            con.Open();
+            String str = "insert into employee (emp_id,emp_Name,emp_Desg,emp_City,emp_Age) values (@id,@nm,@desg,@city,@age)";
+            SqlCommand cmd = new SqlCommand(str, con);
+
+
+            cmd.Parameters.AddWithValue("@id", int.Parse(txtId.Text));
+            cmd.Parameters.AddWithValue("@nm", txtName.Text);
+            cmd.Parameters.AddWithValue("@desg", txtDesg.Text);
+            cmd.Parameters.AddWithValue("@city", txtCity.Text);
+            cmd.Parameters.AddWithValue("@age", int.Parse(txtAge.Text));
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Inserted Sucessfully!!");
+
+            con.Close();
+
+
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\user\source\repos\ADOConnectedDemo1\ADOConnectedDemo1\DatabaseEmp.mdf;Integrated Security=True;User Instance=True");
+            con.Open();
+            String str = "update employee set emp_City = @city where emp_Id = @id";
+            SqlCommand cmd = new SqlCommand(str, con);
+
+            cmd.Parameters.AddWithValue("@id", int.Parse(txtId.Text));
+            cmd.Parameters.AddWithValue("@city", txtCity.Text);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Updated Successfully!!");
+
+            txtName.Text = "";
+            txtDesg.Text = "";
+            txtAge.Text = "";
+
+            con.Close();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\user\source\repos\ADOConnectedDemo1\ADOConnectedDemo1\DatabaseEmp.mdf;Integrated Security=True;User Instance=True");
+            con.Open();
+            String str = "delete from employee where emp_id=@id ";
+            SqlCommand cmd = new SqlCommand(str, con);
+
+            cmd.Parameters.AddWithValue("@id", int.Parse(txtId.Text));
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Deleted Successfully");
+            con.Close();
+
+        }
+
+        private void btnEmp_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\user\source\repos\ADOConnectedDemo1\ADOConnectedDemo1\DatabaseEmp.mdf;Integrated Security=True;User Instance=True");
+            con.Open();
+            String str = "select *from employee";
+            SqlCommand cmd = new SqlCommand(str, con);
+
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                int id = rdr.GetInt32(0);
+                String nm = rdr.GetString(1);
+                String desg = rdr.GetString(2);
+                String city = rdr.GetString(3);
+                int age = rdr.GetInt32(4);
+
+                listBox1.Items.Add("ID= " + id+"  ||  "+  " Name= " + nm+"  ||  " + " Desg= " + desg+"  ||  " + " City = " +city+"  ||  " + " Age = " +age );
+            }
+                con.Close();
+
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click_1(object sender, EventArgs e)
+        {
+            txtId.Text = "";
+            txtName.Text = "";
+            txtDesg.Text = "";
+            txtCity.Text = "";
+            txtAge.Text = "";
+            listBox1.Items.Clear();
+
+        }
+    }
+}
